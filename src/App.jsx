fb186a3ce74afa3d1bf0407cda235ccad23160f7
TamaraFerr <tamarablacksmith@hotmail.com>
2025-03-25 16:58:58 -0300

    FEAT: basic layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import './App.css'
+import styled from 'styled-components'
+import Gatinho from "./assets/gatinho.jpg"
 
 function App() {
   return (
     <>
+      <Container>
+        <h1>Bem-vindo ao aplicativos de gatinhos!!</h1>
+        <h2>Aqui você pode gerar fotos aleatórias de gatos e até mesmo dar nomes a alguns</h2>
+
+
+        <input />
+        <img src={Gatinho} alt="gatinho filhote fofo"/>
+
+        <button>Gerar gato</button>
+      </Container>
     </>
   )
 }
 
 export default App
+
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #E6E6FA;
+`
+
+const Title = styled.p``
+
+const Subtitle = styled.p``
+
+const Input = styled.input``
+
