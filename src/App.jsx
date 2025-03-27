10d7cdeb08dbd22cf893cc0285dbb71bd2a18d48
TamaraFerr <tamarablacksmith@hotmail.com>
2025-03-27 17:16:03 -0300

    FIX: img border and texturing icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import styled from 'styled-components'
 import Gatinho from "./assets/gatinho.jpg"
+import { TiHeartFullOutline } from "react-icons/ti";
 
 function App() {
   return (
     <>
       <Container>
-        <Title>Bem-vindo ao aplicativos de gatinhos!!</Title>
-        <Subtitle>Aqui você pode gerar fotos aleatórias de gatos e até mesmo dar nomes a alguns</Subtitle>
+        <h1>Bem-vindo ao gerador de gatinhos!!</h1>
+        <h2>Aqui você pode gerar fotos aleatórias de gatos e até mesmo dar nomes a alguns</h2>
 
 
         <Input 
           type="text"
           placeholder= "Dê nome ao seu gatinho aqui!"
         />
-        <Img src={Gatinho} alt="gatinho filhote fofo"/>
+
+        <Content>
+          <Img src={Gatinho} alt="gatinho filhote fofo"/>
+          <TiHeartFullOutline 
+            size='50px'
+            color= '#36454F'
+          />
+        </Content>
 
         <Button>Gerar gato</Button>
       </Container>
@@ -25,44 +33,54 @@ export default App
 
 
 const Container = styled.div`
+  box-sizing: border-box;
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: #FCDFFF;
+  background-color: #E6C7C2;
   justify-content: space-evenly;
-`
 
-const Title = styled.p`
-  font-size: 50px;
-  font-weight: 600;
-  font-family: "Montserrat", sans-serif;
-`
+  & h1 {
+    font-size: 50px;
+    font-weight: 600;
+    font-family: "Montserrat", sans-serif;
+    color: #393939;
+  }
 
-const Subtitle = styled.p`
-  font-size: 20px;
-  font-weight: 400;
-  font-family: "Montserrat", sans-serif;
+  & h2 {
+    font-size: 20px;
+    font-weight: 400;
+    font-family: "Montserrat", sans-serif;
+    color: #393939;
+  }
 `
 
 const Input = styled.input`
-  width: 20%;
+  width: 25%;
   height: 35px;
   border-radius: 55px;
   border: 2px solid #FFDFDD;
   box-sizing: border-box;
   padding-left: 15px;
+
+  &:focus {
+    outline: none;
+    border: 3px solid #E56E94;
+  }
 `
 
 const Img = styled.img`
-  width: 30%;
-  height: 40%;
-  border-radius: 200px;
+  width: 400px;
+  height: auto;
+  border-radius: 100px;
+  margin-right: 15px;
+  border: 1px solid #EAEEE9;
 `
 
 const Button = styled.button`
-  width: 150px;
+  width: 200px;
   height: 30px;
   border-radius: 60px;
   border: 2px solid #F6C6BD;
@@ -70,4 +88,20 @@ const Button = styled.button`
   font-size: 15px;
   font-weight: 300;
   color: #E56E94;
+  cursor: pointer;
+  transition: 0.2s;
+
+  &:hover {
+    color: #ffffff;
+    background-color: #E56E94;
+  }
+`
+
+const Content = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: 40%;
 `
